2d83e361826b4bcdbd328fa3e172eba35acf134f
ZIN MYO <overmidnight12@gmail.com>
2019-07-21 13:53:31 -0700

    my changes

---
+++ 010_es6.js
@@ -0,0 +1,11 @@
+const personalInformation = {
+	firstName : 'Nan',
+	lastName : 'Sandar',
+	city : 'Sanchaung',
+	state : 'Yangon',
+	zipCode : 11009
+};
+
+const {firstName: fn, lastName: ln} = personalInformation;
+
+console.log(`${fn} ${ln}`);
--- myScript.js
+++
@@ -1,6 +0,0 @@
-   function myFunction(){
-        document.getElementById("demo").innerHTML = "<span class='alert alert-success'>Paragraph changed.</span>";
-      }
-      function myco(){
-        document.getElementById("demo2").style.background = "yellow";
-      }
