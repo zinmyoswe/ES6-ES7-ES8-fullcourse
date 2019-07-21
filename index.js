6047cc3d1256bfc86e9a1b84f434d5bbac1a5afc
ZIN MYO <overmidnight12@gmail.com>
2019-07-21 13:41:01 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let word1 = 'Thura';
+let word2 = 'Aung';
+let num1 = 2;
+let num2 = 3;
+
+const fullName = `${word1} ${word2}`;
+const haha = `${num1+num2} ${word2}`;
+
+let example = `${word1} ${word2}`;
+
+console.log(fullName);
+console.log(haha);
+console.log(example);
+
+document.getElementById('example').innerText = example;
---
+++ myScript.js
@@ -0,0 +1,6 @@
+   function myFunction(){
+        document.getElementById("demo").innerHTML = "<span class='alert alert-success'>Paragraph changed.</span>";
+      }
+      function myco(){
+        document.getElementById("demo2").style.background = "yellow";
+      }
